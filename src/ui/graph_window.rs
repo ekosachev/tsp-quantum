3eb3417ca4908bfdbd4139cacdc3616fe04e74ca
Egor Kosachev <egorkosachev@vk.com>
2026-03-27 00:01:17 +0300

    feat: add creating nodes

--- src/ui/graph_window.rs
+++ src/ui/graph_window.rs
@@ -1,10 +1,33 @@
-use eframe::egui;
+use eframe::egui::{self};
 
 #[derive(Default)]
 pub struct GraphWindow {
-    pub _nodes: Vec<egui::Pos2>,
+    nodes: Vec<egui::Pos2>,
+    screen_origin: egui::Pos2,
 }
 
 impl GraphWindow {
-    pub fn ui(&mut self, _ui: &mut egui::Ui) {}
+    pub fn ui(&mut self, ui: &mut egui::Ui) {
+        let (response, painter) = ui.allocate_painter(
+            ui.available_size_before_wrap(),
+            egui::Sense::click_and_drag(),
+        );
+        self.screen_origin = painter.clip_rect().left_top();
+        self.logic(&response);
+        self.paint_nodes(&painter);
+    }
+
+    pub fn logic(&mut self, response: &egui::Response) {
+        if let Some(interact_pos) = response.interact_pointer_pos()
+            && response.clicked()
+        {
+            self.nodes.push(interact_pos - self.screen_origin.to_vec2())
+        }
+    }
+
+    fn paint_nodes(&self, painter: &egui::Painter) {
+        self.nodes.iter().for_each(|n| {
+            painter.circle_filled(self.screen_origin + n.to_vec2(), 5.0, egui::Color32::GREEN);
+        });
+    }
 }
