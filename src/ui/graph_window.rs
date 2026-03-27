b92525233d36341e91e5747dd82b5715aefe0e50
Egor Kosachev <egorkosachev@vk.com>
2026-03-27 11:32:27 +0300

    feat: add moving nodes

--- src/ui/graph_window.rs
+++ src/ui/graph_window.rs
@@ -4,6 +4,8 @@ use eframe::egui::{self};
 pub struct GraphWindow {
     nodes: Vec<egui::Pos2>,
     screen_origin: egui::Pos2,
+    hovered_node: Option<usize>,
+    dragged_node: Option<usize>,
 }
 
 impl GraphWindow {
@@ -19,16 +21,24 @@ impl GraphWindow {
     }
 
     fn logic(&mut self, response: &egui::Response) {
-        if let Some(interact_pos) = response.interact_pointer_pos()
-            && response.clicked()
-        {
-            self.nodes.push(interact_pos - self.screen_origin.to_vec2());
-        }
+        self.update_hovered_node(response);
+        self.create_node(response);
+        self.move_node(response);
     }
 
     fn paint_nodes(&self, painter: &egui::Painter) {
         self.nodes.iter().for_each(|n| {
-            painter.circle_filled(self.screen_origin + n.to_vec2(), 5.0, egui::Color32::GREEN);
+            let node_color = self
+                .hovered_node
+                .map(|i| {
+                    if self.nodes[i] == *n {
+                        egui::Color32::LIGHT_GREEN
+                    } else {
+                        egui::Color32::GREEN
+                    }
+                })
+                .unwrap_or(egui::Color32::GREEN);
+            painter.circle_filled(self.screen_origin + n.to_vec2(), 5.0, node_color);
         });
     }
 
@@ -44,5 +54,41 @@ impl GraphWindow {
                 );
             });
         });
+    }
+
+    fn create_node(&mut self, response: &egui::Response) {
+        if let Some(interact_pos) = response.interact_pointer_pos()
+            && response.clicked()
+        {
+            self.nodes.push(interact_pos - self.screen_origin.to_vec2());
+        }
+    }
+
+    fn update_hovered_node(&mut self, response: &egui::Response) {
+        if let Some(hover_pos) = response.hover_pos() {
+            self.hovered_node = self
+                .nodes
+                .iter()
+                .position(|n| hover_pos.distance(self.screen_origin + (*n).to_vec2()) < 10.0);
+        }
+    }
+
+    fn move_node(&mut self, response: &egui::Response) {
+        if self.dragged_node.is_none()
+            && response.drag_started()
+            && let Some(i) = self.hovered_node
+        {
+            self.dragged_node = Some(i);
+        }
+
+        if self.dragged_node.is_some() && response.drag_stopped() {
+            self.dragged_node = None;
+        }
+
+        if let Some(i) = self.dragged_node
+            && response.dragged()
+        {
+            self.nodes[i] += response.drag_delta();
+        }
     }
 }
