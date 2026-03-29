2e2cbd30fa23e8dd44569400acc81a0f972df712
Egor Kosachev <egorkosachev@vk.com>
2026-03-29 22:54:27 +0300

    feat: add heatmap to qubo window

--- src/ui/qubo_window.rs
+++ src/ui/qubo_window.rs
@@ -1,8 +1,21 @@
 use eframe::egui;
+use egui_plot::{Heatmap, Plot};
 
 #[derive(Default)]
 pub struct QuboWindow {}
 
 impl QuboWindow {
-    pub fn ui(&mut self, _ui: &mut egui::Ui) {}
+    pub fn ui(&mut self, ui: &mut egui::Ui) {
+        let heatmap = Heatmap::new(vec![0.0, 0.25, 0.5, 0.75], 2);
+
+        Plot::new("qubo_plot")
+            .allow_zoom(false)
+            .allow_drag(false)
+            .allow_scroll(false)
+            .allow_boxed_zoom(false)
+            .view_aspect(1.0)
+            .show_grid(false)
+            .show_crosshair(false)
+            .show(ui, |plot_ui| plot_ui.heatmap(heatmap));
+    }
 }
